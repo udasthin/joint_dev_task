d2fde97dad64e46015ee852e0c99d21d2110eae7
udagawayuuki mac <udagawayuukimac@udagawayuukinoMacBook-Pro.local>
2020-03-10 15:18:20 +0900

    Q4,8,10,11を修正しました

--- task.rb
+++ task.rb
@@ -29,7 +29,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-sports.delete(nil)
+sports.compact!
 p sports
 end
 
@@ -62,7 +62,7 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  programming_languages.map(&:capitalize)
+  programming_languages.map(&:capitalize!)
   upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
@@ -84,9 +84,9 @@ def q10
 
   # 以下に回答を記載
   foods.each do |food|
-    if food == "うに"
+    if food.include?("うに")
       puts "好物です"
-    else 
+    else
       puts "まぁまぁ好きです"
     end
   end
@@ -96,7 +96,8 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-  sports = sports.flatten
+  sports.flatten!
+  sports.uniq!
   sports.each.with_index(1) do |sport,i|
     puts "NO.#{i} #{sport}"
   end
